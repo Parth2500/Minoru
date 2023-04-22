7aca45ddb887a756bd46d52afc15e876f821e4a6
Parth <work.parthm257@gmail.com>
2023-04-22 20:25:59 +0530

    Initial commit

---
+++ apps/minoru/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'minoru-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'minoru';
+}
